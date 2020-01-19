01b6a21040058c9428e786a1f36a05918903092c
jorchg <rushcentro@gmail.com>
2020-01-19 21:04:20 +0100

    Added babel and uglifier

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 export default [
@@ -12,8 +14,12 @@ export default [
 			format: 'umd'
 		},
 		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+      babel({
+        exclude: 'node_modules/**',
+      }),
+			resolve(),
+      commonjs(),
+      uglify(),
 		]
 	},
 
@@ -25,7 +31,6 @@ export default [
 	// `file` and `format` for each target)
 	{
 		input: 'src/main.js',
-		external: ['ms'],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
