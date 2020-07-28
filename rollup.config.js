fa38514a6db0c7599632c2cfd58fdd851ac9f2dc
jorchg <rushcentro@gmail.com>
2020-07-28 14:54:10 +0200

    Updated packages and fixed build errors on node 12 and 14

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
